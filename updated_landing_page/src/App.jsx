1757800c10f7aefe193f4c983a29bb4e3033a259
vikashrao5464 <vikashrao5464@gmail.com>
2025-07-01 21:56:09 +0530

    made changes to landing page

--- updated_landing_page/src/App.jsx
+++ updated_landing_page/src/App.jsx
@@ -15,6 +15,7 @@ function App() {
         <div className="App">
           <Navbar />
           <Routes>
+            
             <Route path="/" element={<LandingPage />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/signin" element={<SignIn />} />
