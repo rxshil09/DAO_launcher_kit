4465c382285cfe4b12c68b56d2dff49d42d55c39
Rushil Jain <168427743+rxshil09@users.noreply.github.com>
2025-07-24 21:02:50 +0530

    Delete node_modules directory

--- node_modules/dao_frontend/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { fileURLToPath, URL } from 'url';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import environment from 'vite-plugin-environment';
-
-export default defineConfig({
-  plugins: [react()],
-});
