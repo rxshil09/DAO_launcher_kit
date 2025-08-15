f67b37a63e3659f655eabd4613cce33b8ba08176
vikashrao5464 <vikashrao5464@gmail.com>
2025-08-15 22:35:54 +0530

    error fixing

--- updated_landing_page/src/context/AuthContext.jsx
+++ updated_landing_page/src/context/AuthContext.jsx
@@ -6,6 +6,7 @@ const AuthContext =createContext();
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) {
+    
     throw new Error('useAuth must be used within an AuthProvider');
   }
   return context;
